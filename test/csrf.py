fdc467d242dfbd1fb1c57d117882a35154a66e5d
melonattacker <junkitombob@gmail.com>
2023-08-06 17:14:33 +0900

    ADD: demo

---
+++ test/csrf.py
@@ -0,0 +1,85 @@
+import sys
+import os
+import time
+
+from osbtlib import BrowserSimulator, Osbtlib
+
+# Test Information
+test_name = "CSRF"
+test_description = "csrf description"
+outcome = "failed"
+err_msg = ""
+countermeasure = "csrf countermeasure"
+
+HONEST_RP_ENDPOINT = "http://localhost:9999"
+HONEST_OP_ENDPOINT = "http://localhost:9998"
+PROXY_SERVER_ENDPOINT = "http://localhost:8080"
+PROXY_EXTENSION_ENDPOINT = "http://localhost:5555"
+
+osbt = Osbtlib(
+    proxy_extension_url = PROXY_EXTENSION_ENDPOINT
+)
+
+# victim credentials
+victim_username = 'test-user@localhost'
+victim_password = 'verysecure'
+
+# attacker credentials
+attacker_username = 'test-user2'
+attacker_password = 'verysecure'
+
+try:
+    # add intercept rule
+    osbt.proxy.intercept_response(f"{HONEST_RP_ENDPOINT}/auth/callback?code=")
+    
+    # login with victim account
+    sso_flow = f"""
+page.locator('input[name="username"]').fill('{victim_username}')
+page.locator('input[name="password"]').fill('{victim_password}')
+page.locator('button[type="submit"]').click()
+print(page.content())
+    """
+    bs1 = BrowserSimulator(f'{HONEST_RP_ENDPOINT}/login?issuer={HONEST_OP_ENDPOINT}/', PROXY_SERVER_ENDPOINT)
+    bs1.run(sso_flow)
+    bs1.close()
+
+    # replace location header
+    traces = osbt.proxy.get_history()
+    print("=====================================")
+    for res in traces['response']:
+        for name, value in res['headers'].items():
+            if name == 'Location':
+                location = value
+    
+    osbt.proxy.clean()
+
+    # login with attacker account
+    sso_flow = f"""
+page.locator('input[name="username"]').fill('{attacker_username}')
+page.locator('input[name="password"]').fill('{attacker_password}')
+page.locator('button[type="submit"]').click()
+print(page.content())
+    """
+    bs2 = BrowserSimulator(f'{HONEST_RP_ENDPOINT}/login?issuer={HONEST_OP_ENDPOINT}/', PROXY_SERVER_ENDPOINT)
+    bs2.run(sso_flow)
+
+    # visit location
+    print("location:", location)
+    bs2.visit(location)
+    content = bs2.get_content()
+    print("content:", content)
+    bs2.close()
+
+    # result check
+    if "failed to get state" in content:
+        outcome = "pass"
+    
+    osbt.cli.send_result(test_name, test_description, outcome, err_msg, countermeasure)
+
+except Exception as e:
+    print('Error:', e)
+    osbt.proxy.clean()
+
+    outcome = "failed"
+    err_msg = str(e)
+    osbt.cli.send_result(test_name, test_description, outcome, err_msg, countermeasure)
---
+++ test/id_spoofing.py
@@ -0,0 +1,66 @@
+import sys
+import os
+import time
+import jwt
+
+from osbtlib import BrowserSimulator, Osbtlib
+
+# Test Information
+test_name = "IDSpoofing"
+test_description = "- The attacker op modifies the id_token to impersonate the victim <br> - The sub claim of the id_token is modified to the victim's sub claim"
+outcome = "failed"
+err_msg = ""
+countermeasure = "- Check the signature of the id_token <br> - Check the iss claim of the id_token <br> - Check the sub claim of the id_token"
+
+
+ATTACKER_OP_ENDPOINT = "http://localhost:9997"
+HONEST_RP_ENDPOINT = "http://localhost:9999"
+PROXY_SERVER_ENDPOINT = "http://localhost:8080"
+
+osbt = Osbtlib(
+    attacker_op_url = ATTACKER_OP_ENDPOINT
+)
+
+bs = BrowserSimulator(f'{HONEST_RP_ENDPOINT}/login?issuer={ATTACKER_OP_ENDPOINT}/', PROXY_SERVER_ENDPOINT)
+
+try:
+    # create malicious id_token
+    malicious_id_token = jwt.encode({"issuer": "http://localhost:9997/", "sub": "hoge"}, key="", algorithm="none")
+
+    # send order to attacker op
+    res = osbt.attacker_op.replace_id_token(malicious_id_token)
+    print("request sent:", res)
+
+    time.sleep(5)
+
+    # victim credentials
+    victim_username = 'test-user@localhost'
+    victim_password = 'verysecure'
+
+    # browser simulation
+    sso_flow = f"""
+page.locator('input[name="username"]').fill('{victim_username}')
+page.locator('input[name="password"]').fill('{victim_password}')
+page.locator('button[type="submit"]').click()
+print(page.content())
+    """
+    
+    bs.run(sso_flow)
+    content = bs.get_content()
+    print("content:", content)
+    bs.close()
+
+    # result check
+    if "issuer does not match" in content:
+        outcome = "pass"
+
+    osbt.attacker_op.clean()
+
+    osbt.cli.send_result(test_name, test_description, outcome, err_msg, countermeasure)
+except Exception as e:
+    print('Error:', e)
+    osbt.attacker_op.clean()
+
+    outcome = "failed"
+    err_msg = str(e)
+    osbt.cli.send_result(test_name, test_description, outcome, err_msg, countermeasure)
